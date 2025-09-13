6e56d26b00d25bb63acd31bad42ea36d43d23810
kiran685 <sskiran961@gmail.com>
2025-09-13 17:11:57 +0530

    react

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,49 +1,45 @@
-import ProductCart from "./ProductCart";
-import BlogPost from "./BlogPost.jsx";
-
+import "./App.css"
+import {useState} from "react"
 function App() {
-    const products = [
-        { productname: "Laptop", price: 50000, isAvailable: true },
-        { productname: "Smartphone", price: 20000, isAvailable: false },
-        { productname: "Headphones", price: 3000, isAvailable: true },
-        { productname: "Smartwatch", price: 7000, isAvailable: false }
-    ];
+    const[count,setCount]=useState(0)
+    const[email,setEmail]=useState("")
+    const[password,setPassword]=useState("")  
+   const handleClick=(name)=>{
+     alert("Login Successful")
+   }
+   const handleSubmit=(event)=>{
+     event.preventDefault();
+     alert(`Email:${email}\nPassword:${password}\nYou are logged in`)
+   }
+   const handleChange=(event)=>{
+     console.log(event.target.name)
+     console.log(event.target.value)
+   }
 
-    const posts = [
-        {
-            id:1,
-            author: "Kiran",
-            title: "My first blog",
-            description: "This is my first blog post content"
-        },
-        {
-            id:2,
-            author: "Teja",
-            title: "My second blog",
-            description: "This is my second blog post content"
-        },
-        {
-            id:3,
-            author: "Sai",
-            title: "My third blog",
-            description: "This is my third blog post content"
-        },
-    ];
+  const handleEmail=(event)=>{
+    setEmail(event.target.value)
+  }
+  const handlePassword=(event)=>{
+    setPassword(event.target.value)
+  }
+  const handleIncrement=()=>{
+    setCount((prevCount)=>prevCount+1)
+    console.log(count)
+  }
+  
+  return (
+    <div>
+      {<form onSubmit={handleSubmit}>
+      <p>Iam a button</p>
+      <input type="text" name="Email" placeholder="Enter your Email" onChange={handleChange}/><br/>
+      <input type="password" name="password" placeholder="Enter your password" onChange={handleChange}/><br/>
+      <button type="Submit">Login</button>
+      </form> }
+      <p>{count}</p>
+      <button onClick={handleIncrement}>Increment</button>
+    </div>
+  )
 
-    return (
-        <div style={{ display: 'flex', gap: '20px' }}>
-            {posts.map((post) => (
-                <BlogPost
-                    key={post.id}
-                    author={post.author}
-                    title={post.title}
-                    description={post.description}
-                />
-            ))}
-        </div>
-    );
 }
 
-export default App;
-
-
+export default App
